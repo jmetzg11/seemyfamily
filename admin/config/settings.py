f3556b2ac8857ed969780396970957fd516ba3ae
jmetzg11 <jmetzg11@gmail.com>
2026-08-18 21:16:04 -0500

    Remove legacy code

--- admin/config/settings.py
+++ admin/config/settings.py
@@ -75,7 +75,4 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR.parent / 'media'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
